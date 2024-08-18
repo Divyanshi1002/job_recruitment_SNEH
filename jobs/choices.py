e9ec4b84479f35835a0e387404a2a65c0ba860e3
Divyanshi1002 <157373233+Divyanshi1002@users.noreply.github.com>
2024-08-18 10:59:03 +0000

    Added Folders

---
+++ jobs/choices.py
@@ -0,0 +1,11 @@
+location_choices = {
+    "Nainital": "Nainital",
+    "Haldwani": "Haldwani",
+    "Rudrapur": "Rudrapur",
+    "Almora": "Almora"
+}
+
+contract_choices = {
+    "Part Time": "Part Time",
+    "Freelance": "Freelance"
+}
